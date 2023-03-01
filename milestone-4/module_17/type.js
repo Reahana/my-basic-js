e5497d0aec7062e924d8cdc719b830adc9956225
Reahana <reahana233@gmail.com>
2023-03-01 13:21:12 +0600

    type

---
+++ milestone-4/module_17/type.js
@@ -0,0 +1,16 @@
+var price = 100;
+
+// console.log(typeof price);
+var price = "100";
+
+var nothing;
+
+// console.log(typeof nothing);
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+
+sum = sum.toFixed(1);
+sum = parseFloat(sum)
+console.log(sum);
