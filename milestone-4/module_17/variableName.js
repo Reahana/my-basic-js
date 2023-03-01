578c2f4c33ca2480bd059b92e53620ab7c068bcb
Reahana <reahana233@gmail.com>
2023-03-01 13:21:21 +0600

    variable name

---
+++ milestone-4/module_17/variableName.js
@@ -0,0 +1,24 @@
+var price = 29 ;
+
+
+//  * 1. variable name cannot be any keyword
+var false = 12;
+ 
+//  * 2. variable name will be in one word
+var my home = "Dhaka";
+//  * 3. variable name cannot have qutation
+var "user name" = "Reahana";
+//  * 4. variable name cannot start with number
+var 1no = 10;
+//  * 5. variable name cannot use dash
+var user-name = "Amanda"
+ 
+
+  var user_name = "Amanda"; //use underscore (snakecase)
+  var userName = "Amanda"; // use camelcase  : we will use it 
+  var UserName = "Amanda"; // use pascalcase
+
+// variable name is  case sensitive
+var Num = 10;
+var NUM = 11;
+var num =12;
