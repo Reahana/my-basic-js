5e54bdbd0c4669ce9c5950f0b84f81c26df62ada
Reahana <reahana233@gmail.com>
2023-03-01 12:09:15 +0600

    math that we have to know

---
+++ milestone-4/module_17/math.js
@@ -0,0 +1,16 @@
+/**
+ * 1. number ==> 55 , 88888 , -32
+ * 2. integer
+ * 3. float
+ * 4. operation : add , sub , multi , division , reminder
+ * 5. percent
+ * 6. average
+ * 7. comparision : > , < , == , != , >=, <=
+ * 8. ঐকিক
+ * 9. max, min , average , sum
+ * 10 . series
+ * 11 odd ,even
+ * 12 prime number
+ * 13 permutation
+ * 14. different geomatry (extra)
+ */
