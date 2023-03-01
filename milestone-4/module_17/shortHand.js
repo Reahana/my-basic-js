aafc229f6f51eccc90611749e0ce43de3b65c793
Reahana <reahana233@gmail.com>
2023-03-01 13:20:52 +0600

    short hand

---
+++ milestone-4/module_17/shortHand.js
@@ -0,0 +1,18 @@
+var tomato = 25; //variable declaration
+ tomato = 30 ; // variable update
+
+ //add something to the previous value
+
+ var price1 = 31;
+ var price2 = 10;
+
+ price1= price1 + 10;
+
+ console.log(price1);
+
+ // shorthand +=, -= ,*=, /=
+ price1 += 10;
+
+
+ var age = 15
+ age ++ ; // adding one with existing value
