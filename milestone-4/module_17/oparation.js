eab077b3a49ce3e1efbfc0677f5d5dd8dadd5194
Reahana <reahana233@gmail.com>
2023-03-01 13:20:34 +0600

    oparation

---
+++ milestone-4/module_17/oparation.js
@@ -0,0 +1,28 @@
+var eggPrice = 10;
+var onionPrice = 20;
+
+//multiply
+var orangePrice = 20;
+var quality = 7;
+
+var allorange = orangePrice * quality;
+console.log(allorange);
+
+//division
+var money = 500;
+var player = 10
+
+var eachOne = money / player;
+console.log(eachOne);
+
+//addition
+var totalPrice = onionPrice + eggPrice;
+//subtraction
+var priceDifference = onionPrice - eggPrice;
+
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice + eggPrice ); 
+
+// console.log(totalPrice);
+// console.log(priceDifference);
