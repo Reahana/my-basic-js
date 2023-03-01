e12a2ab1ee8cf6ae2f8979d273d2323be28ad38d
Reahana <reahana233@gmail.com>
2023-03-01 13:20:15 +0600

    concate

---
+++ milestone-4/module_17/concate.js
@@ -0,0 +1,9 @@
+var price1 = 31;
+var price2 = 10;
+var total = price1 + price2;
+
+var firstName = "Reahana";
+var secondName = "Gomes";
+
+var fullName = firstName +' '+ secondName;
+console.log(fullName);
