b239e6892036cfa43cbd89063d7b6c45b90ecbcf
Reahana <reahana233@gmail.com>
2023-03-03 19:18:08 +0600

    index , indexOf

--- milestone-4/module_18/array.js
+++ milestone-4/module_18/array.js
@@ -1,8 +1,19 @@
 var friendAge = [11,21,45,17,41, 6];
 var picnicFee = [5000,2000,4000,150];
 
-var fruite = ["apple", "banana", "mango","orange"];
+var fruite = ["apple", "banana", "mango","licchi","orange","papaya"];
 
-console.log(fruite);
-console.log(picnicFee.length);
-console.log(fruite.indexOf(apple));
+// console.log(fruite);
+// console.log(picnicFee.length);
+
+// index 
+//console.log(fruite[3]);
+
+
+var newFruite = fruite[3];
+
+// fruite[3] = "jack-fruit"
+// console.log(fruite);
+
+
+console.log(fruite.indexOf("jack-fruit"));
