6b1d410a1cd4e4a9c5c5ab8297a51d5f411688c9
Reahana <reahana233@gmail.com>
2023-03-03 19:42:04 +0600

    if else

---
+++ milestone-4/module_18/conditional.js
@@ -0,0 +1,13 @@
+var iphone = 79000;
+var money = 5000;
+
+// if (iphone < money) {
+//     console.log('Hurray');
+// }
+
+if (iphone < money) {
+    console.log('Hurray');
+}
+else{
+console.log('bye');
+}
