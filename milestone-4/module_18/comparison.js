92ca1db7dbae8306fad9a6104853f61d2466a9ba
Reahana <reahana233@gmail.com>
2023-03-03 19:34:43 +0600

    comparision

---
+++ milestone-4/module_18/comparison.js
@@ -0,0 +1,12 @@
+//console.log(5 < 6);
+//console.log(5 > 6);
+//console.log(16 <= 6);
+//console.log(5 != 6);
+
+var num1 = 12;
+var num2 = 50;
+console.log(num1 > num2);
+
+//multiple conditions
+//  &&  all condition should fulfil
+//  ||  any condition should fulfil
