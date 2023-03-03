0d0827c8bc8e36d9f6dca7faff5ba615b443f936
Reahana <reahana233@gmail.com>
2023-03-03 19:26:35 +0600

    push pop

---
+++ milestone-4/module_18/pushPop.js
@@ -0,0 +1,13 @@
+var fruit = ["apple", "banana", "mango","licchi","orange","papaya"];
+
+//push  it means add a element in the last 
+// fruit.push("grape");
+// fruit.push("guava");
+
+
+//pop it means remove the last element
+console.log(fruit);
+fruit.pop();
+var myFruit = fruit.pop();
+console.log(fruit);
+console.log(myFruit);
