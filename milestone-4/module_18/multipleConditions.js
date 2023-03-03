5917123d35f2b8d255ee08ab572f72b0e4144b51
Reahana <reahana233@gmail.com>
2023-03-03 19:53:22 +0600

    multiple condition

---
+++ milestone-4/module_18/multipleConditions.js
@@ -0,0 +1,28 @@
+var isGrad = true;
+var salary = 5000;
+car =1;
+
+// if (isGrad == true) {
+//     console.log("Be Happy");
+// }
+// else{
+//     console.log("Bye");
+// }
+// if (isGrad == true && salary>50000) {
+//     console.log("Be Happy");
+// }
+// else{
+//     console.log("Bye");
+// }
+// if (isGrad == true || salary>50000 ) {
+//     console.log("Be Happy");
+// }
+// else{
+//     console.log("Bye");
+// }
+if ((isGrad == true && salary>50000) || car >1 ) {
+    console.log("Be Happy");
+}
+else{
+    console.log("Bye");
+}
