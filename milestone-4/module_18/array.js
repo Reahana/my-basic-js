7f1862670a6f749dc9b72453f09538440ec50052
Reahana <reahana233@gmail.com>
2023-03-03 19:10:09 +0600

    array

---
+++ milestone-4/module_18/array.js
@@ -0,0 +1,8 @@
+var friendAge = [11,21,45,17,41, 6];
+var picnicFee = [5000,2000,4000,150];
+
+var fruite = ["apple", "banana", "mango","orange"];
+
+console.log(fruite);
+console.log(picnicFee.length);
+console.log(fruite.indexOf(apple));
