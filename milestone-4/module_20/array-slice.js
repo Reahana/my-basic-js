b7cee43671b210ea7518985f3fe1ec146f705933
Reahana <reahana233@gmail.com>
2023-03-08 11:40:02 +0600

    add js

---
+++ milestone-4/module_20/array-slice.js
@@ -0,0 +1,3 @@
+var numbers = [1,8,6,12,85,133,69,58];
+var num = numbers.slice(2,6);
+console.log(num);
---
+++ milestone-4/module_20/string.js
@@ -0,0 +1,3 @@
+var Name = "My name is Reahana";
+console.log(Name.includes('has'));
+console.log(Name.substring(1,15));
