5240d8482c58df9ea0d1290d42cb3812ef34fd94
Reahana <reahana233@gmail.com>
2023-03-08 11:08:14 +0600

    subject math

---
+++ milestone-4/module_19/subject.js
@@ -0,0 +1,10 @@
+var math = 75.25
+var biology = 65;
+var bangla = 99.58
+var chemestry =80;
+var english = 60.96;
+
+var total = math+biology+bangla+chemestry+english
+var  average = total/5;
+
+console.log(parseFloat(average.toFixed(2)));
