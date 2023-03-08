afd73e77ab3ecc9ba09b8a219de60c437f80eed1
Reahana <reahana233@gmail.com>
2023-03-08 13:30:37 +0600

    object

---
+++ milestone-4/module_20/object.js
@@ -0,0 +1,11 @@
+var student = {
+    id: 11,
+    name : 'Asrafi',
+    class:10,
+    marks:100,
+    color: ['black','blue','white']
+};
+console.log(student);
+console.log(student.id);
+console.log(student.color.indexOf('blue'));
+console.log(student.color);
