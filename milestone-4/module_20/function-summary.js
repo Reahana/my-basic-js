50f8febea863140dba4354de166d0fec34e42da8
Reahana <reahana233@gmail.com>
2023-03-08 13:03:23 +0600

    summary

---
+++ milestone-4/module_20/function-summary.js
@@ -0,0 +1,13 @@
+// function functionName(parameters,arg) {
+//     function body
+//     return
+// }
+
+function myMath(a,b,c,d) {
+    const total = a+b+c+d;
+    const average = total/4;
+    return average;
+}
+
+var myAverage =  myMath(10,25,92.02);
+console.log(myAverage);
