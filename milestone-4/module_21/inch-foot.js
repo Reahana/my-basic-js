82b58eba4721d16ec34799ade993462a60fe8560
Reahana <reahana233@gmail.com>
2023-03-08 17:13:35 +0600

    inch feet

---
+++ milestone-4/module_21/inch-foot.js
@@ -0,0 +1,12 @@
+const myInches = 12
+const myFeet = myInches/12;
+
+//console.log(myFeet);
+
+function inchToFeeth(i)
+{
+    var f = i/12
+    return f;
+}
+var feet = inchToFeeth(168)
+console.log(feet);
