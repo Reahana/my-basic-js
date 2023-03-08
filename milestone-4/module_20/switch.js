ef7317c8c80db254b109e12556d28175786716ed
Reahana <reahana233@gmail.com>
2023-03-08 16:48:10 +0600

    switch

---
+++ milestone-4/module_20/switch.js
@@ -0,0 +1,17 @@
+
+const color = 'yellow';
+switch (color) {
+    case 'red':
+console.log('red bro');
+        break;
+case 'blue':
+    console.log('blue bro');
+    case 'yellow':
+        console.log('yellow bro');
+                break;
+        case 'green':
+            console.log('green bro');
+    default:
+         console.log('no bro');
+        break;
+}
