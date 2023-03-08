326bb6941dde80d041ee0012e7fdd99fb40bd599
Reahana <reahana233@gmail.com>
2023-03-08 17:26:12 +0600

    odd even true false

---
+++ milestone-4/module_21/odd_even.js
@@ -0,0 +1,14 @@
+function oddEven(n)
+{
+    if(n%2==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+let result = oddEven(566633321);
+console.log(result);
