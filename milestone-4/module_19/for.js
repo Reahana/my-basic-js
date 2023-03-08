6ff1590e9bf52add6a7ec5336b7511f93aa01665
Reahana <reahana233@gmail.com>
2023-03-08 11:06:45 +0600

    for loop

---
+++ milestone-4/module_19/for.js
@@ -0,0 +1,13 @@
+
+// for (var roast= 0; roast<7; roast++) {
+//     console.log("please give me roast");
+// }
+
+// for (var i= 0; i<7; i++) {
+//     console.log(i);
+// }
+var sum =0;
+for(var i =0 ; i<=3; i++){
+   sum =sum + i;
+   console.log(sum);
+}
