b2969a96a3e578f287f2714d687337dbcecb4245
Reahana <reahana233@gmail.com>
2023-03-09 17:45:39 +0600

    string

---
+++ milestone-4/module_22/string.js
@@ -0,0 +1,11 @@
+let str = 'JavaScript awesome ';
+console.log(str.length);
+console.log(str[0]);
+let first = 'ema'
+let last = 'ok'
+let full = first +last;
+console.log(full);
+first = first + 'doll'
+console.log(first);
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
